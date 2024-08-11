7d1fccd7c26bef49834db3eb29153a2c26d60580
J M <jonas04mair@gmail.com>
2024-08-11 17:41:10 +0200

    finaler Stand vom elften achten, can comm implementiert, mnoch fehlerhaft

--- MDK-ARM/ADC_Software/system_stm32f4xx.d
+++ MDK-ARM/ADC_Software/system_stm32f4xx.d
@@ -2,20 +2,20 @@ adc_software/system_stm32f4xx.o: ..\Src\system_stm32f4xx.c \
   ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
   ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f405xx.h \
   ..\Drivers\CMSIS\Include\core_cm4.h \
-  D:\Programme\ D\ARM\ARMCLANG\include\stdint.h \
-  D:\Schulisches\Starkstrom\ Formula\ Stundent\Federwegsensoren\Jonas_Lukas_ADC_Software\Software_FWS_ADC_V0\Drivers\CMSIS\Include\cmsis_version.h \
-  D:\Schulisches\Starkstrom\ Formula\ Stundent\Federwegsensoren\Jonas_Lukas_ADC_Software\Software_FWS_ADC_V0\Drivers\CMSIS\Include\cmsis_compiler.h \
-  D:\Schulisches\Starkstrom\ Formula\ Stundent\Federwegsensoren\Jonas_Lukas_ADC_Software\Software_FWS_ADC_V0\Drivers\CMSIS\Include\cmsis_armclang.h \
-  D:\Programme\ D\ARM\ARMCLANG\include\arm_compat.h \
-  D:\Programme\ D\ARM\ARMCLANG\include\arm_acle.h \
-  D:\Schulisches\Starkstrom\ Formula\ Stundent\Federwegsensoren\Jonas_Lukas_ADC_Software\Software_FWS_ADC_V0\Drivers\CMSIS\Include\mpu_armv7.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Dev\Starkstrom\ADC_Software\Drivers\CMSIS\Include\cmsis_version.h \
+  C:\Dev\Starkstrom\ADC_Software\Drivers\CMSIS\Include\cmsis_compiler.h \
+  C:\Dev\Starkstrom\ADC_Software\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  C:\Dev\Starkstrom\ADC_Software\Drivers\CMSIS\Include\mpu_armv7.h \
   ..\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h \
   ..\Inc\stm32f4xx_hal_conf.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_def.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\Legacy\stm32_hal_legacy.h \
-  D:\Programme\ D\ARM\ARMCLANG\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc_ex.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_gpio.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_gpio_ex.h \
--- Src/main.c
+++ Src/main.c
@@ -22,6 +22,7 @@
 #include "spi.h"
 #include "tim.h"
 #include "gpio.h"
+#include "hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGINIncludes */
@@ -101,8 +102,12 @@ int main(void)
   MX_TIM7_Init();
   
 	/* USER CODE BEGIN 2 */
+	HAL_TIM_Base_Start_IT(&htim7);
+	
 	can_init(); 									// Can initialisieren
 	adcStartup();
+	
+	HAL_GPIO_WritePin(GPIOA, nSYNC_nRESET_PIN, true);
 	
 	//HAL_GPIO_WritePin(LED_HEARTBEAT_GPIO_Port, LED_HEARTBEAT_Pin, GPIO_PIN_SET);
   /* USER CODE END 2 */
@@ -114,7 +119,7 @@ int main(void)
     /* USER CODE END WHILE */
 			TxData_ADC[7] = 0xFF;
 			can_send_ADC(TxData_ADC);
-			delay_ms(500);
+			//delay_ms(500);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
